e19f311a31f8f7e25d2dea1ba4564bac400ae4fb
James Stevenson <everamzah@gmail.com>
2016-03-12 00:14:00 -0500

    Super pick for any node

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 minetest.register_tool("superpick:pick", {
 	description = "Super Pickaxe",
-	inventory_image = "default_tool_mesepick.png^default_mese_crystal_fragment.png",
+	inventory_image = "default_tool_mesepick.png^default_obsidian_shard.png",
 	range = 11,
 	groups = {not_in_creative_inventory = 1},
 	tool_capabilities = {
@@ -22,6 +22,7 @@ minetest.register_tool("superpick:pick", {
 
 minetest.register_privilege("superpick", {description = "Ability to wield the mighty admin pickaxe!"})
 
+local resisters = {}
 local function kill_node(pos, node, puncher)
 	if puncher:get_wielded_item():get_name() == "superpick:pick" then
 		if not minetest.check_player_privs(
@@ -51,3 +52,27 @@ end
 minetest.register_on_punchnode(function(pos, node, puncher)
 	kill_node(pos, node, puncher)
 end)
+
+	minetest.after(0.01, function ()
+		minetest.after(0.01, function ()
+for node in pairs(minetest.registered_nodes) do
+	local def = minetest.registered_nodes[node]
+	for i in pairs(def) do
+		if i == "on_punch" then
+
+local rem = def.on_punch
+local function new_on_punch(pos, node, puncher, pointed_thing)
+	kill_node(pos, node, puncher)
+	return rem(pos, node, puncher, pointed_thing)
+end
+
+minetest.override_item(node, {
+	on_punch = new_on_punch
+})
+		end
+	end
+end
+		end)
+	end)
+
+print("hi")  -- This doesn't print.
